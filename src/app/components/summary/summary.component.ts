ae0ec2ac7f4ef40323b60a54b591d5fe137e9595
Andile <andilelethumusa@gmail.com>
2025-04-28 10:32:26 +0200

    Count projects per manager method fixed

--- src/app/components/summary/summary.component.ts
+++ src/app/components/summary/summary.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectService } from '../../services/project.service';
-import { ManagerSummary, Project } from '../../interfaces/project';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { Chart,registerables } from 'chart.js';
-import { log } from 'console';
-Chart.register(...registerables)
+import { ManagerSummary, Project, User } from '../../interfaces/project';
+import { Chart, registerables } from 'chart.js';
+import { UserService } from '../../services/user.service';
+Chart.register(...registerables);
 @Component({
   selector: 'app-summary',
   standalone: true,
@@ -16,13 +15,20 @@ export class SummaryComponent {
   projects: Project[] = [];
   projectSummary: ManagerSummary[] = [];
   sum!: ManagerSummary;
-  
+  managers: User[] = [];
+
   constructor(
     private projServ: ProjectService,
+    private userServ: UserService
   ) {}
 
   ngOnInit(): void {
+    this.userServ.getManagers().subscribe((resp: User[]) => {
+      this.managers = resp;
+      this.renderChart();
+    });
     this.projServ.getProjects().subscribe((resp: Project[]) => {
+      this.projects = resp;
       for (let i = 0; i < resp.length; i++) {
         const proj = resp[i];
         let managerFound = false;
@@ -34,61 +40,56 @@ export class SummaryComponent {
           if (projSum.email === proj.manager) {
             managerFound = true;
             break;
+          } else {
+            countProjects = this.countProjPerManager('andilecsir@gmail.com');
           }
         }
 
         if (!managerFound) {
           this.projectSummary.push({
             email: proj.manager!,
-            totalProjects:2,
+            totalProjects: countProjects,
           });
         }
-        
       }
-      
     });
   }
-  
 
-  ngAfterContentInit(){
-    //console.log("SADASDSADSADAS",this.projectSummary.map((email)=>email))
-    this.renderChart()
-  }
-
-  managerSummary(email: string): number {
+  countProjPerManager(email: string): number {
     let count = 0;
-    for (let index = 0; index < this.projects.length; index++) {
-      const element = this.projects[index];
-
-      if (element.manager === email) {
+    this.projects.forEach((element) => {
+      if (element.manager == email) {
         count++;
       }
-    }
+    });
+
     return count;
   }
 
-  renderChart(){
-    const labels = this.projectSummary.map((email)=>email);
-    const values = this.projectSummary.map((totalProjects)=> totalProjects);
-    new Chart("myChart",{
-      type:'bar',
-      data:{
-        labels:["andile","Kane","siwe"],
-        datasets:[{
-          label: 'No of projects assigned per Manager',
-          data:[3,4,1],
-          
-        }]
+  renderChart() {
+    const labels = this.projectSummary.map((sum) => sum.email.split('@')[0]);
+    console.log('LABELS', labels);
+    const values = this.projectSummary.map((totalProjects) => totalProjects);
+    new Chart('myChart', {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: 'No of projects assigned per Manager',
+            data: [3, 4, 1],
+          },
+        ],
       },
-      options:{
+      options: {
         responsive: true,
-        plugins:{
+        plugins: {
           legend: {
             display: true,
-            position: 'right'
-          }
-        }
-      }
-    })
+            position: 'bottom',
+          },
+        },
+      },
+    });
   }
 }
